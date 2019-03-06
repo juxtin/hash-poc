a79cc04fe50ab1d7726e97b1396fdc3dffdde1be
Justin Holguin <juxtin@github.com>
2019-03-06 10:32:43 -0800

    Cleanup and extend benchmarks

--- src/Hash.hs
+++ src/Hash.hs
@@ -2,21 +2,21 @@ module Hash
     ( digestnLazy
     , digestnStrict
     , digest2n
+    , digestnLazyFile
+    , digestnStrictFile
+    , digest2nFile
     ) where
 
 import qualified Crypto.Hash.SHA512 as SHA512
-import qualified Data.ByteString as B
-import qualified Data.ByteString.Lazy as BSL
 import qualified Data.ByteString.Char8 as B8
 import qualified Data.Text as T
 import qualified Data.Text.Lazy as TL
+import qualified Data.Text.Lazy.IO as TLIO
+import qualified Data.Text.IO as TIO
 import qualified Data.Text.Encoding as TE
 import qualified Data.ByteString.Base16 as B16
 
 type Filter = SHA512.Ctx -> Char -> SHA512.Ctx
-
-idFilter :: Filter
-idFilter ctx char = SHA512.update ctx $ B8.singleton char
 
 toFilter :: (Char -> Bool) -> Filter
 toFilter f ctx c =
@@ -25,20 +25,20 @@ toFilter f ctx c =
   else ctx
 
 digestnLazy' :: Filter -> TL.Text -> B8.ByteString
-digestnLazy' filter text =
+digestnLazy' f text =
   B16.encode $ SHA512.finalize ctx
   where
-    ctx = TL.foldl filter ctx0 text
+    ctx = TL.foldl f ctx0 text
     ctx0 = SHA512.init
 
 digestnLazy :: (Char -> Bool) -> TL.Text -> B8.ByteString
 digestnLazy f = digestnLazy' (toFilter f)
 
 digestnStrict' :: Filter -> T.Text -> B8.ByteString
-digestnStrict' filter text =
+digestnStrict' f text =
   B16.encode $ SHA512.finalize ctx
   where
-    ctx = T.foldl filter ctx0 text
+    ctx = T.foldl f ctx0 text
     ctx0 = SHA512.init
 
 digestnStrict :: (Char -> Bool) -> T.Text -> B8.ByteString
@@ -46,3 +46,12 @@ digestnStrict f = digestnStrict' (toFilter f)
 
 digest2n :: (Char -> Bool) -> T.Text -> B8.ByteString
 digest2n f t = B16.encode $ SHA512.hash $ TE.encodeUtf8 $ T.filter f t
+
+digestnLazyFile :: FilePath -> (Char -> Bool) -> IO B8.ByteString
+digestnLazyFile path f = digestnLazy f <$> TLIO.readFile path
+
+digestnStrictFile :: FilePath -> (Char -> Bool) -> IO B8.ByteString
+digestnStrictFile path f = digestnStrict f <$> TIO.readFile path
+
+digest2nFile :: FilePath -> (Char -> Bool) -> IO B8.ByteString
+digest2nFile path f = digest2n f <$> TIO.readFile path
--- src/Main.hs
+++ src/Main.hs
@@ -3,30 +3,67 @@ module Main where
 import           Criterion.Main
 import qualified Data.Text      as T
 import qualified Data.Text.Lazy as TL
+import qualified Data.Text.Lazy.IO as TLIO
 import qualified Hash
 
 nLazy = Hash.digestnLazy (const True)
 nStrict = Hash.digestnStrict (const True)
 d2n = Hash.digest2n (const True)
 
-exampleLazy n = TL.replicate n $ TL.pack "Lorem ipsum and stuff"
-exampleStrict n = T.replicate n $ T.pack "Lorem ipsum and stuff"
+nLazyIO = flip Hash.digestnLazyFile (const True)
+nStrictIO = flip Hash.digestnStrictFile (const True)
+d2nIO = flip Hash.digest2nFile (const True)
+
+exampleLazy n = TL.pack $ unlines $ replicate n "Lorem ipsum and stuff"
+exampleStrict n = T.pack $ unlines $ replicate n "Lorem ipsum and stuff"
+
+writeExampleFile n = do
+  let contents = exampleLazy n
+  let path = "/tmp/hash-poc-example-" ++ show n
+  TLIO.writeFile path contents
+  return path
+
 
 main :: IO ()
 main = do
+  file1 <- writeExampleFile 1
+  file10 <- writeExampleFile 10
+  file100 <- writeExampleFile 100     -- about 2k
+  file1000 <- writeExampleFile 1000   -- about 21k
+  file10000 <- writeExampleFile 10000 -- about 215k
   defaultMain [ bgroup "lazy" [ bench "1" $ whnf nLazy (exampleLazy 1)
                               , bench "10" $ whnf nLazy (exampleLazy 10)
                               , bench "100" $ whnf nLazy (exampleLazy 100)
-                              , bench "1000" $ whnf nLazy (exampleLazy 1000)
+                              , bench "10000" $ whnf nLazy (exampleLazy 10000)
                               ]
               , bgroup "strict" [ bench "1" $ whnf nStrict (exampleStrict 1)
                                 , bench "10" $ whnf nStrict (exampleStrict 10)
                                 , bench "100" $ whnf nStrict (exampleStrict 100)
                                 , bench "1000" $ whnf nStrict (exampleStrict 1000)
+                                , bench "10000" $ whnf nStrict (exampleStrict 10000)
                                 ]
               , bgroup "2n" [ bench "1" $ whnf d2n (exampleStrict 1)
                             , bench "10" $ whnf d2n (exampleStrict 10)
                             , bench "100" $ whnf d2n (exampleStrict 100)
                             , bench "1000" $ whnf d2n (exampleStrict 1000)
+                            , bench "10000" $ whnf d2n (exampleStrict 10000)
                             ]
+              , bgroup "lazyIO" [ bench "1" $ nfIO $ nLazyIO file1
+                                , bench "10" $ nfIO $ nLazyIO file10
+                                , bench "100" $ nfIO $ nLazyIO file100
+                                , bench "1000" $ nfIO $ nLazyIO file1000
+                                , bench "10000" $ nfIO $ nLazyIO file10000
+                                ]
+              , bgroup "strictIO" [ bench "1" $ nfIO $ nStrictIO file1
+                                  , bench "10" $ nfIO $ nStrictIO file10
+                                  , bench "100" $ nfIO $ nStrictIO file100
+                                  , bench "1000" $ nfIO $ nStrictIO file1000
+                                  , bench "10000" $ nfIO $ nStrictIO file10000
+                                  ]
+              , bgroup "2nIO" [ bench "1" $ nfIO $ d2nIO file1
+                              , bench "10" $ nfIO $ d2nIO file10
+                              , bench "100" $ nfIO $ d2nIO file100
+                              , bench "1000" $ nfIO $ d2nIO file1000
+                              , bench "10000" $ nfIO $ d2nIO file10000
+                              ]
               ]
